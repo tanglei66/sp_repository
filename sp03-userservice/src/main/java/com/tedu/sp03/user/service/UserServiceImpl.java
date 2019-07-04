d21139a0c8aade93256debcb1cebd6303680aad5
Administrator <Administrator@PC201207261146>
2019-07-04 11:30:57 +0800

    1

--- sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
+++ sp03-userservice/src/main/java/com/tedu/sp03/user/service/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package com.tedu.sp03.user.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -11,7 +11,10 @@ import com.tedu.sp01.service.UserService;
 import com.tedu.web.util.JsonUtil;
 
 import lombok.extern.slf4j.Slf4j;
-
+/*
+ * 只有添加了@RefreshScope注解Bean实例，才会被重新注入新的配置
+ */
+@RefreshScope
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
